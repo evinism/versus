639c4eb7bf7ad528b959b3f65d9ae6ec93f43139
Evin Sellin <evinism@gmail.com>
2019-06-04 20:25:36 -0700

    remove elm stuff, basic model

--- src/Main.elm
+++ src/Main.elm
@@ -6,13 +6,33 @@ import Html.Events exposing (onClick)
 
 
 type alias Model =
-    { plainText : String }
+    { plainText : String
+    , mapping : List Char
+    }
+
+
+-- base helpers!!! --
+
+mappingToString : List Char -> String
+mappingToString mapping = List.map (String.fromChar) mapping |> (String.join "")
+
+--obfuscateSingleCharacter : List Char -> Char -> Char
+--obfuscateSingleCharacter mapping char = Char.toCode 
+--    |> \code -> if ( || )
+--    |>  Char.fromCode
+
+--toObfuscatedText : String -> List Char -> String
+--toObfuscatedText plainText mapping = split |> mapSingleChar mapping |> join
+
+-- 
 
 
 initialModel : Model
 initialModel =
-    { plainText = "Hey I'm a Plain Text" }
-
+    { plainText = "Hey I'm a Plain Text"
+    -- Implicitly associated by position to a, b, c, ...
+    , mapping = ['i', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'a', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    }
 
 type Msg = Yeet
 
@@ -24,6 +44,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ div [] [ text <| model.plainText ]
+        , div [] [ text <| mappingToString model.mapping ]
         ]
 
 
