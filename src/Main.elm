f2b6aab574d9fe3d1a61a4439f106502b6854f9e
Evin Sellin <evinism@gmail.com>
2019-06-04 20:52:56 -0700

    create and consume swap message

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import List.Extra
 
 
 type alias Model =
@@ -34,17 +35,38 @@ initialModel =
     , mapping = ['i', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'a', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     }
 
-type Msg = Yeet
+type Msg = Swap Char Char
+
+
+--- helpers for update!!!
+
+-- stupid stuff
+findCharPos : Char -> List Char -> Int
+findCharPos toFind mapping = (List.Extra.elemIndex toFind mapping)
+    |> Maybe.withDefault 0 -- We assume this is never hit
+
+
+swapChars : Char -> Char -> List Char -> List Char
+swapChars firstChar secondChar mapping = 
+    let 
+        firstCharPos = findCharPos firstChar mapping
+        secondCharPos = findCharPos secondChar mapping
+    in List.Extra.swapAt firstCharPos secondCharPos mapping
+
+--- 
 
 update : Msg -> Model -> Model
-update msg model = model
-
+update msg model =
+    case msg of
+        Swap char1 char2 ->
+            { model | mapping = swapChars char1 char2 model.mapping }
 
 view : Model -> Html Msg
 view model =
     div []
         [ div [] [ text <| model.plainText ]
         , div [] [ text <| mappingToString model.mapping ]
+        , button [ onClick (Swap 'a' 'i') ] [ text "yeet" ]
         ]
 
 
